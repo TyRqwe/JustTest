b01a9f1ea15a9a5e48714c9936c8f9a30dbd7ac6
TyRqwe <35452160+TyRqwe@users.noreply.github.com>
2026-04-29 00:35:35 +0300

    Update 02_single_thread_test.sh

--- 02_single_thread_test.sh
+++ 02_single_thread_test.sh
@@ -27,7 +27,8 @@ chmod 644 "$TXN_FILE"
 OUTPUT_FILE="/tmp/pgbench_single.out"
 start_time=$(date +%s)
 echo "Запуск pgbench (прогресс каждые 10 сек)..."
-timeout $TIMEOUT_SEC sh -c "sudo -u postgres pgbench -d '$DB_NAME' -f '$TXN_FILE' -c 1 -j 1 -t $TARGET_TRANSACTIONS -P 10 -n 2>&1 | grep -v '^pgbench: client' | grep -vE '^(SET|UPDATE|INSERT|DELETE|SELECT)|col5 =|WHERE id =' | tee '$OUTPUT_FILE'"
+# stdbuf отключает буферизацию для немедленного вывода
+timeout $TIMEOUT_SEC sh -c "sudo -u postgres stdbuf -oL -eL pgbench -d '$DB_NAME' -f '$TXN_FILE' -c 1 -j 1 -t $TARGET_TRANSACTIONS -P 10 -n 2>&1 | grep -v '^pgbench: client' | grep -vE '^(SET|UPDATE|INSERT|DELETE|SELECT)|col5 =|WHERE id =' | tee '$OUTPUT_FILE'"
 EXIT_CODE=$?
 end_time=$(date +%s)
 elapsed=$((end_time - start_time))
